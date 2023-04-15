763cd5fa72d0bcfb31e9fcd0d7f3a936a81da74d
gereacosta1 <107889277+gereacosta1@users.noreply.github.com>
2023-04-15 16:00:19 -0300

    detalle-varias-pages

---
+++ javascript/detalle.js
@@ -0,0 +1,52 @@
+// let productos = [
+//     {
+//       codigo: "vehiculo1",
+//       nombre: "Cubierta Michelin",
+//       precio: "$40000",
+//       descripcion: "Tamaño y rodado: 255/70 R16 y Carga máxima: 1090 kg",
+//       categoria: "Accesorios para vehiculos",
+//       stock: 8,
+//     },
+//   ];
+
+
+//   // let titulo = document.getElementById("titulo-producto")
+
+//   // let titulo = document.createElement("p")
+//   // titulo.innerText = productos.nombre
+//   // document.body.appendChild(div)//manda adentro de la etiqueta padre 
+
+
+
+//    productos.forEach(function (producto) {
+//     document.write(
+//         `<b>Nombre:</b> ${producto.nombre}<br />`
+//     );
+//     document.write(
+//       `<b>precio:</b> ${producto.precio}<br />`
+//    );
+//    document.write(
+//     `<b>descripcion:</b> ${producto.descripcion}<br />`
+//   );
+//     document.write(
+//       `<b>categoria:</b> ${producto.categoria}<br />`
+//   );
+//    document.write(
+//     `<b>stock:</b> ${producto.stock}<br />`
+//   );
+//    document.write(
+//   `<b>codigo:</b> ${producto.codigo}<br />`
+//   );
+// });
+
+
+
+// document.getElementById("datos-producto").innerHTML = "div"
+
+//     let div = document.createElement("div")
+//     div.classList= "container"
+
+//     let estructura = `<p>productos.nombre</p>`
+
+//     div.innerHTML = estructura
+//     document.getElementById("datos-producto").appendChild(div)
