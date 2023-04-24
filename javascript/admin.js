f6416c5dc30b8a25226b33f9d95848aa501f4d8b
¨´Nadir <elochay@gmail.com>
2023-04-24 20:04:08 -0300

    arreglodelAdmin

--- javascript/admin.js
+++ javascript/admin.js
@@ -1,17 +1,13 @@
 let list = JSON.parse(localStorage.getItem("lista")) || [];
 let h1 = document.querySelector("h1");
 h1.innerText = "Administrador";
-
 // if (localStorage.getItem("logged", false)) { 
 //     window.location = "../pages/login.html";
 // }
-
 let h3 = document.querySelector("h3");
 h3.innerText = "Productos";
-
 let button = document.querySelector("button");
 button.innerText = "AGREGAR PRODUCTO";
-
 let table = document.querySelector("table");
 let tbody = document.querySelector("tbody");
 
@@ -39,9 +35,27 @@ const actualizarTabla = () => {
     let listaMapeada = list.map((fila, indice) => `<tr><td>${fila.id}</td><td>${fila.codigo}</td><td>${fila.categoria}</td><td>${fila.nombre}</td><td>${fila.precio}</td><td>${fila.stock}</td><td><button onclick="">Detalle</button></td><td><button onclick="editarFila(${indice})">Editar</button></td><td><button onclick="eliminarFila(${indice})">Eliminar</button></td></tr>`);
     tbody.innerHTML = listaMapeada.join("");
 }
-const editarFila = () => {    
-    let listaMapeada = list.map(fila => `<tr><td contenteditable>${fila.id}</td><td contenteditable>${fila.codigo}</td><td contenteditable>${fila.categoria}</td><td contenteditable>${fila.nombre}</td><td contenteditable>${fila.precio}</td><td contenteditable>${fila.stock}</td><td><button onclick="">Detalle</button></td><td><button onclick="editarFila()">Editar</button></td><td><button onclick="eliminarFila()">Eliminar</button></td></tr>`);
-    tbody.innerHTML = listaMapeada.join().replaceAll(",", "")
+const editarFila = (indice) => {    
+    let listaMapeada = list.map((fila, i) => {
+        if (i === indice) {
+            return `<tr><td contenteditable>${fila.id}</td><td contenteditable>${fila.codigo}</td><td contenteditable>${fila.categoria}</td><td contenteditable>${fila.nombre}</td><td contenteditable>${fila.precio}</td><td contenteditable>${fila.stock}</td><td><button onclick="">Detalle</button></td><td><button onclick="guardarFila(${indice})">Guardar</button></td><td><button onclick="eliminarFila(${indice})">Eliminar</button></td></tr>`;
+        } else {
+            return `<tr><td>${fila.id}</td><td>${fila.codigo}</td><td>${fila.categoria}</td><td>${fila.nombre}</td><td>${fila.precio}</td><td>${fila.stock}</td><td><button onclick="">Detalle</button></td><td><button onclick="editarFila(${i})">Editar</button></td><td><button onclick="eliminarFila(${i})">Eliminar</button></td></tr>`;
+        }
+    });
+    tbody.innerHTML = listaMapeada.join("");
+}
+const guardarFila = (indice) => {
+    let filaEditada = tbody.children[indice];
+    let id = filaEditada.children[0].textContent;
+    let codigo = filaEditada.children[1].textContent;
+    let categoria = filaEditada.children[2].textContent;
+    let nombre = filaEditada.children[3].textContent;
+    let precio = filaEditada.children[4].textContent;
+    let stock = filaEditada.children[5].textContent;
+    list[indice] = { id, codigo, categoria, nombre, precio, stock };
+    localStorage.setItem("lista", JSON.stringify(list));
+    actualizarTabla();
 }
 const eliminarFila = (indice) => {
     let confirmacion = confirm("¿Seguro que deseas eliminar este producto?");
--- javascript/login.js
+++ javascript/login.js
@@ -5,19 +5,18 @@ form.addEventListener("submit", (event) => {  //realizara un evento en especific
 	event.preventDefault();
 	let email = document.getElementById("email").value;
 	let password = document.getElementById("password").value;
-	let user = arrayUsuarios.find(usuario => usuario.emailActual === email && usuario.passwordActual === password);
-	if (user) {
-		window.location.href = "../index.html";
-		alert("Bienvenido " + user.nameActual + "!");
-	} else if (email === "elochay24@gmail.com" && password === "4321") {
+	if (email === "elochay24@gmail.com" && password === "4321") {
 		window.location.href = "../pages/admin.html";
 		alert("Bienvenido administrador");
-	    localStorage.setItem("logged", yes);
+		localStorage.setItem("logged", "yes");
 	} else {
-		alert("Los datos ingresados son incorrectos");
-		window.location.href = "../pages/login.html";
+		let user = arrayUsuarios.find(usuario => usuario.emailActual === email && usuario.passwordActual === password);
+		if (user) {
+			window.location.href = "../index.html";
+			alert("Bienvenido " + user.nameActual + "!");
+		} else {
+			alert("Los datos ingresados son incorrectos");
+			window.location.href = "../pages/login.html";
+		}
 	}
 });
-
-
-
